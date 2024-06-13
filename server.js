2f1c4963de269f62d7da7df54e3624172c361833
GicheruBenjamin <projectbuild201@gmail.com>
2024-06-13 12:40:21 +0300

    C12.40

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => 
-    res.send('I am a king in express')
-)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+  app.use("/public", express.static(__dirname + "/public"));
 
 // Require the upload middleware
 const upload = require('./upload');
