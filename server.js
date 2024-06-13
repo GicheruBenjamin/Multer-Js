879de2f62c7f64b9a032d869bbc10a5a33409fc1
GicheruBenjamin <projectbuild201@gmail.com>
2024-06-13 12:18:51 +0300

    C12.18

---
+++ server.js
@@ -0,0 +1,10 @@
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => res.send('I am a king in express'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
